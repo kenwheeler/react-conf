71d818dee6264994b4e8c3b000865bb970f0d3fa
Ken Wheeler <ken_wheeler@me.com>
2017-03-11 15:16:04 -0500

    updates

--- presentation/index.js
+++ presentation/index.js
@@ -37,7 +37,9 @@ const images = {
   rn: require('../assets/react-native.png'),
   cli: require('../assets/cli.gif'),
   gamekit: require('../assets/gamekit.gif'),
-  westworld: require('../assets/westworld.png')
+  westworld: require('../assets/westworld.png'),
+  spectacle: require('../assets/spectacle.gif'),
+  music: require('../assets/music.gif')
 };
 
 preloader(images);
@@ -87,7 +89,7 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["none"]} theme={theme} progress="bar">
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Heading size={1} caps lineHeight={1} textColor="secondary">
             <StrangeTop>
               <StrangeLarge>U</StrangeLarge>SIN<StrangeLarge>G</StrangeLarge>
@@ -102,33 +104,24 @@ export default class Presentation extends React.Component {
             <StrangeText>FOR ANYTHING BUT WEBSITES</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Image src={images.logo} width="100%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '7rem', textTransform: 'uppercase'}}>
             <StrangeText>Remember the first time you saw react?</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '5rem', margin: "0px 0px 30px 0px", textTransform: 'uppercase'}}>
-            <StrangeText>IS THAT HTML IN MY JS?</StrangeText>
-          </Text>
-          <CodePane lang="javascript" textSize={36} source={`render() {
-  return (
-    <div>{this.props.whatever}</div>
-  )
-}`}/>
-        </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Image src={images.disgusting} />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
-            <StrangeText>No, that's JSX and these are components</StrangeText>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
+          <Text textColor="tertiary" style={{fontSize: '3rem', margin: "0px 0px 30px 0px", textTransform: 'uppercase'}}>
+            <StrangeText>I changed my mind after using it</StrangeText>
           </Text>
+          <Image src={images.beavis} />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text margin="10px 0 0" textColor="tertiary" style={{fontSize: '3rem', margin: "20px 0px", textTransform: 'uppercase'}}>
             <StrangeText>Components are the</StrangeText>
           </Text>
@@ -142,26 +135,20 @@ export default class Presentation extends React.Component {
             <StrangeText>almost anything</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '3rem', margin: "0px 0px 30px", textTransform: 'uppercase'}}>
-            <StrangeText>Productivity like whoa</StrangeText>
-          </Text>
-          <Image src={images.beavis} />
-        </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Image src={images.rn} width="100%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>Historically, our JS would operate on our markup</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>With React, we own our output</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '3rem', margin: "0px 0px 30px"}}>
             <StrangeText>LETS TALK ABOUT BUILDING SOME</StrangeText>
           </Text>
@@ -176,12 +163,13 @@ export default class Presentation extends React.Component {
             </StrangeBottom>
           </Heading>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '7rem', textTransform: 'uppercase'}}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
+          <Text textColor="tertiary" style={{fontSize: '5rem', margin: '0px 0px 30px', textTransform: 'uppercase'}}>
             <StrangeText>Spectacle</StrangeText>
           </Text>
+          <Image src={images.spectacle} width="50%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>JSX Based Presentation Library</StyledListItem>
             <StyledListItem>Uses context for theming and control</StyledListItem>
@@ -189,13 +177,13 @@ export default class Presentation extends React.Component {
             <StyledListItem>Still technically web markup</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', margin: "0px 0px 30px 0px", textTransform: 'uppercase'}}>
             <StrangeText>VICTORY CLI</StrangeText>
           </Text>
           <Image src={images.cli} width="75%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>Uses Victory React Charting Library</StyledListItem>
             <StyledListItem>Victory outputs valid SVG</StyledListItem>
@@ -203,32 +191,32 @@ export default class Presentation extends React.Component {
             <StyledListItem>SVG is output or converted to png</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '7rem', textTransform: 'uppercase'}}>
             <StrangeText>react-fs-renderer</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>Use JSX to scaffold out files and dirs</StyledListItem>
             <StyledListItem>Traverses component tree</StyledListItem>
             <StyledListItem>Creates fs entries based upon markup</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', margin: "0px 0px 30px 0px", textTransform: 'uppercase'}}>
             <StrangeText>react-game-kit</StrangeText>
           </Text>
           <Image src={images.gamekit} width="75%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>Use JSX to describe game logic</StyledListItem>
             <StyledListItem>Passes tick via context</StyledListItem>
             <StyledListItem>Maintains adjacent physics world with MatterJS</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Heading size={1} caps lineHeight={1} textColor="secondary">
             <StrangeTop>
               <StrangeLarge>R</StrangeLarge>ende<StrangeLarge>r</StrangeLarge>
@@ -243,19 +231,20 @@ export default class Presentation extends React.Component {
             <StrangeText>COMPONENT METHOD</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>Renders a noop element, like a span</StyledListItem>
             <StyledListItem>Output is in browser, but not DOM tree</StyledListItem>
             <StyledListItem>Access to lifecycle methods/updates</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '7rem', textTransform: 'uppercase'}}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
+          <Text textColor="tertiary" style={{fontSize: '5rem', margin: '0px 0px 30px', textTransform: 'uppercase'}}>
             <StrangeText>react-music</StrangeText>
           </Text>
+          <Image src={images.music} width="50%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>Use JSX to write music</StyledListItem>
             <StyledListItem>Web Audio API</StyledListItem>
@@ -265,20 +254,7 @@ export default class Presentation extends React.Component {
             <StyledListItem>Creates chain of AudioNodes</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <Text textColor="tertiary" style={{fontSize: '7rem', textTransform: 'uppercase'}}>
-            <StrangeText>gl-react (v3)</StrangeText>
-          </Text>
-        </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <List>
-            <StyledListItem>OpenGL/WebGL bindings for React</StyledListItem>
-            <StyledListItem>React, React Native & Headless</StyledListItem>
-            <StyledListItem>Also uses context</StyledListItem>
-            <StyledListItem>Renders to a drawing context</StyledListItem>
-          </List>
-        </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Heading size={1} caps lineHeight={1} textColor="secondary">
             <StrangeTop>
               <StrangeLarge>C</StrangeLarge>usto<StrangeLarge>M</StrangeLarge>
@@ -293,19 +269,19 @@ export default class Presentation extends React.Component {
             <StrangeText>VIA INJECTION METHOD</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <List>
             <StyledListItem>React Native</StyledListItem>
             <StyledListItem>react-blessed</StyledListItem>
             <StyledListItem>react-hardware</StyledListItem>
           </List>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>YOU CAN INJECT OVERRIDES INTO REACT INTERNALS</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <CodePane lang="javascript" textSize={24} source={`import ReactInjection from 'react/lib/ReactInjection';
 
 function inject() {
@@ -314,38 +290,30 @@ function inject() {
   );
 }`}/>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>This allows you to write your own renderer</StrangeText>
           </Text> 
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
-          <List>
-            <StyledListItem>render() function</StyledListItem>
-            <StyledListItem>Batching strategy</StyledListItem>
-            <StyledListItem>Transaction</StyledListItem>
-            <StyledListItem>mount</StyledListItem>
-          </List>
-        </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>FUTURE</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Image src={images.westworld} width="75%" />
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>Fiber: first class custom rendering API</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>You can do all this with react, but should you?</StrangeText>
           </Text>
         </Slide>
-        <Slide bgImage={images.bg} bgDarken={0.5}>
+        <Slide bgImage={images.bg} bgDarken={0.25}>
           <Text textColor="tertiary" style={{fontSize: '5rem', textTransform: 'uppercase'}}>
             <StrangeText>Yes. Go Have Fun!</StrangeText>
           </Text>
